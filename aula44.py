50efe8d04b094253d86e96d84196bdc0dc25ba6d
Majari <majaandressa@hotmail.com>
2026-03-26 15:07:59 -0300

    Adicionando estruturacao das informacoes dentro do nosso backend

--- aula44.py
+++ aula44.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
 meus_livroz = {}
+
+class Livro(BaseModel):
+    nome_livro: str
+    autor_livro: str
+    ano_livro: int
 
 @app.get("/")
 def hello_world():
@@ -16,25 +23,20 @@ def get_livros():
         return {"livros": meus_livroz}
     
 @app.post("/adiciona")
-def post_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def post_livros(id_livro: int, livro: Livro):
     if id_livro in meus_livroz:
         raise HTTPException(status_code=400, detail="Esse livro já está cadastrado!")
     else:
-        meus_livroz[id_livro] = {"nome_livro": nome_livro, "autor_livro": autor_livro, "ano_livro": ano_livro}
+        meus_livroz[id_livro] = livro.model_dump()
         return {"message": "O livro foi adicionado com sucesso!"}
     
 @app.put("/atualiza/{id_livro}")
-def put_livros(id_livro: int, nome_livro: str, autor_livro: str, ano_livro: int):
+def put_livros(id_livro: int, livro: Livro):
     meu_livro = meus_livroz.get(id_livro)
     if not meu_livro:
         raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
     else:
-        if nome_livro:
-            meu_livro["nome_livro"] = nome_livro
-        if autor_livro:
-            meu_livro["autor_livro"] = autor_livro
-        if ano_livro:
-            meu_livro["ano_livro"] = ano_livro
+        meu_livro[id_livro] = livro.model_dump()
         
         return {"message": "As informações do seu livro foram atualizadas com sucesso!"}
 
